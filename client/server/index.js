aff139d601659ba50e37c8eb535c52362485b711
prathammongia <pratham.mongia2022@vitsyudent.ac.in>
2026-03-27 11:47:57 +0530

    Backend ready for deployment

--- client/server/index.js
+++ client/server/index.js
@@ -131,6 +131,8 @@ ${data.driveLink || "Not Provided"}
 });
 
 // Start Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
